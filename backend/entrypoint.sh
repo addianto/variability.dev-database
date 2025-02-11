8263ddb1beb3c5d505fde1159756b1afbae28535
CyberSleeper <mahartha.gemilang@gmail.com>
2025-02-11 17:05:03 +0700

    attempting to dockerize

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,9 +1,5 @@
-#!/bin/sh
-
+#!/bin/bash
 while ! nc -zv db 5432; do echo "Connecting" && sleep 1; done;
-
 python manage.py makemigrations
-# python manage.py migrate --run-syncdb
 python manage.py migrate
 gunicorn --bind ":8000" --workers 3 ddueruemweb.wsgi:application
-
